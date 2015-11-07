6c2be2a4fc4359600584d16a135f451fc2d51a10
David Kang <david@ErChino.local>
2015-11-07 00:18:52 +0000

    Added server

--- lita-todo.rb
+++ lita-todo.rb
@@ -1,12 +1,21 @@
 module Lita
   module Handlers
     class Todo < Handler
-      route(/^\/list$/, :index, command: true, help: {"/list" => "List all todos."})
+      config :server
+      route(/^remind plz$/, :index, command: true, help: {"/list" => "List all todos."})
 
       def index(response)
-        response.reply('You have done all the todos! Good job!')
+        todos = parse get("#{config.server}/todos.json")
+        if todos.any?
+          response.reply('Your todos:')
+          todos.each do |todo|
+            # resonse.reply("#{todo['due']} - #{todo['title']}")
+            response.reply("# #{todo['id']}: #{todo['due']} - #{todo['title']}")
+          end
+        else
+          response.reply('You have done all the todos! Good job!')
+        end
       end
-
 
       private
 
