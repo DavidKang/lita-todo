78230956ed427e3929fcff93dc7c836c62d5ddf6
David Kang <david@ErChino.local>
2015-11-13 18:35:26 +0000

    Added create params

--- Gemfile
+++ Gemfile
@@ -6,3 +6,7 @@ gem 'lita-slack'
 #For Joke ^^
 gem 'lita-chuck_norris'
 gem "lita-catgif"
+
+gem 'pry-rails'
+gem 'pry-byebug'
+gem 'awesome_print'
--- lita-todo.rb
+++ lita-todo.rb
@@ -3,16 +3,22 @@ module Lita
     class Todo < Handler
       config :server
       route(/^remind plz$/, :index, command: true, help: {"remind plz" => "List all todos."})
+      route(/^pending plz$/, :pending, command: true, help: {"pending plz" => "List pending todos."})
       route(/^create\s(.+)$/, :create, command: true, help: {"create" => "create TODO"})
+      route(/^today\s(.+)$/, :today, command: true, help: {"create" => "list today TODO"})
+      route(/^done\s(.+)$/, :done, command: true, help: { "done TODO_ID" => "Marks todo with the specified number as done." })
+      route(/^reopen\s(.+)$/, :reopen, command: true, help: { "done TODO_ID" => "Marks todo with the specified number as done." })
+      route(/^\s(.+)$/, :done, command: true, help: { "done TODO_ID" => "Marks todo with the specified number as done." })
 
       def index(response)
         todos = parse get("#{config.server}/todos.json")
         if todos.any?
-          response.reply('Your todos:')
-          todos.each do |todo|
-            # resonse.reply("#{todo['due']} - #{todo['title']}")
-            response.reply("# #{todo['id']}: #{todo['due']} - #{todo['title']}")
-          end
+          todo = ["```"]
+          todo << 'Your todos:'
+          todos.map { |t| todo << "##{t['id']}: #{t['due']} - #{t['title']}" }
+          todo << "```"
+          todo = todo.join("\n")
+          response.reply(todo)
         else
           response.reply('You have done all the todos! Good job!')
         end
@@ -20,11 +26,22 @@ module Lita
 
       def create(response)
         todo = response.match_data[1]
-        result = post "#{config.server}/todos.json", {'todo[title]' => todo, 'todo[due]' => Date.today }
+        todo = Hash[*todo.split('##').map { |a| a.gsub(/^([a-z]+):/,"todo[\\1]:").split(':') }.flatten]
+        result = post "#{config.server}/todos.json", todo
         if result.code.to_i.success?
           response.reply("#{todo} was created")
         else
-          response.reply()
+          #TODO
+        end
+      end
+
+      def update_state(response)
+        todo = response.match_data[1].split()
+        result = put "#{config.server}/todos/#{todo[0]}/edit.json", { 'todo[status]' => todo[1] }
+        if result.code.to_i.success?
+          response.reply("Todo ##{todo[0]} was updated to #{todo[1]}")
+        else
+          #TODO
         end
       end
 
@@ -36,6 +53,10 @@ module Lita
 
       def post(url, data = {})
         Net::HTTP.post_form(make_uri(url), data)
+      end
+
+      def put(url, data = {})
+        Net::HTTP.put(make_uri(url), data)
       end
 
       def parse(obj)
